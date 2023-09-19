79c592f410651e2df69e73e050f53ead33d4e6a6
Jonatas nascimento <contato.martinsjonatas@gmail.com>
2023-09-19 09:05:32 -0300

    Initial commit

---
+++ src/Turista.java
@@ -0,0 +1,25 @@
+public class Turista {
+    private String nome;
+    private String cpf;
+
+    //Encapsulamentos - Getters e Setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+
+
+
+}
